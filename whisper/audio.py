501a80639de5a63f6cbcfa5170204356e7a4a4f0
Kumuthu Edirisinghe <91903331+kumuthu53@users.noreply.github.com>
2022-10-13 19:19:57 +1100

    Update app.py

--- whisper/audio.py
+++ whisper/audio.py
@@ -41,7 +41,7 @@ def load_audio(file: str, sr: int = SAMPLE_RATE):
         out, _ = (
             ffmpeg.input(file, threads=0)
             .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-            .run(cmd=["ffmpeg", "-nostdin"], capture_stdout=True, capture_stderr=True)
+            .run_async(pipe_stdout=True, cmd=r"/usr/local/bin/ffmpeg/ffmpeg")
         )
     except ffmpeg.Error as e:
         raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
